b5364506461e26c538a0fb33367f07715e8a6115
hemillainysantos <hemillainy.santos@ccc.ufcg.edu.br>
2018-07-25 23:47:09 -0300

    .

--- src/main/java/com/sistemaprematricula/controllers/CurricularComponentController.java
+++ src/main/java/com/sistemaprematricula/controllers/CurricularComponentController.java
@@ -2,6 +2,8 @@ package com.sistemaprematricula.controllers;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,7 +28,8 @@ public class CurricularComponentController {
 		return this.curricularComponentService.getAll();
 
 	}
-
+	
+	@Transactional
 	@RequestMapping(value = "/save", method = RequestMethod.POST)
 	public CurricularComponent save(@RequestBody CurricularComponent curricularComponent) {
 		return this.curricularComponentService.save(curricularComponent);
